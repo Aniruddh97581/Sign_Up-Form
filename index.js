e23895a5c4c7cb00a21b9b47de1b41bc0cbe2ec8
Aniruddh <aniruddhgoel1996@gmail.com>
2020-10-07 20:02:43 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+function validate(){
+    var first_name = document.getElementById("first_name").value;
+    var last_name = document.getElementById("last_name").value;
+    var email = document.getElementById("email").value;
+    var dob = document.getElementById("dob").value;
+    var phone = document.getElementById("phone").value;
+    var password = document.getElementById("password").value;
+    var error_message = document.getElementById("error_message");
+    
+    error_message.style.padding = "10px";
+    
+    var text;
+    if(first_name.length < 5){
+      text = "Please Enter valid Name";
+      error_message.innerHTML = text;
+      return false;
+    }
+    if(last_name.length < 5){
+        text = "Please Enter valid Name";
+        error_message.innerHTML = text;
+        return false;
+    }
+    if(isNaN(phone) || phone.length != 10){
+      text = "Please Enter valid Phone Number";
+      error_message.innerHTML = text;
+      return false;
+    }
+    if(email.indexOf("@") == -1 || email.length < 6){
+      text = "Please Enter valid Email";
+      error_message.innerHTML = text;
+      return false;
+    }
+    alert("Form Submitted Successfully!");
+    return true;
+  }
